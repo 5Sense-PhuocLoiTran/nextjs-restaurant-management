11358562f6f448f5ad9f0842d568a80539bbd569
Tran Phuoc Loi <87872808+5Sense-PhuocLoiTran@users.noreply.github.com>
2025-06-18 15:36:30 +0700

    handle add account modal

--- src/app/(loggedIn)/manage/accounts/add-employee.tsx
+++ src/app/(loggedIn)/manage/accounts/add-employee.tsx
@@ -13,6 +13,9 @@ import {
 import { Form, FormField, FormItem, FormMessage } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { handleErrorApi } from '@/lib/utils'
+import { useAddAccountMutation } from '@/queries/useAccount'
+import { useUploadMediaMutation } from '@/queries/useMedia'
 import {
   CreateEmployeeAccountBody,
   CreateEmployeeAccountBodyType,
@@ -21,11 +24,16 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { PlusCircle, Upload } from 'lucide-react'
 import { useMemo, useRef, useState } from 'react'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 
 export default function AddEmployee() {
   const [file, setFile] = useState<File | null>(null)
   const [open, setOpen] = useState(false)
   const avatarInputRef = useRef<HTMLInputElement | null>(null)
+
+  const addAccountMutation = useAddAccountMutation()
+  const uploadMediaMutation = useUploadMediaMutation()
+
   const form = useForm<CreateEmployeeAccountBodyType>({
     resolver: zodResolver(CreateEmployeeAccountBody),
     defaultValues: {
@@ -44,6 +52,40 @@ export default function AddEmployee() {
     }
     return avatar
   }, [file, avatar])
+
+  const resetForm = () => {
+    form.reset()
+    setFile(null)
+  }
+
+  const onSubmit = async (values: CreateEmployeeAccountBodyType) => {
+    let body = values
+    try {
+      if (file) {
+        const formData = new FormData()
+        formData.append('file', file)
+        const uploadAvatarResult = await uploadMediaMutation.mutateAsync(
+          formData
+        )
+        const imageUploadUrl = uploadAvatarResult.payload.data
+        body = {
+          ...values,
+          avatar: imageUploadUrl,
+        }
+      }
+
+      const result = await addAccountMutation.mutateAsync(body)
+
+      if (result) {
+        toast.success(result.payload.message || 'Tạo tài khoản thành công')
+      }
+
+      resetForm() // reset the form
+      setOpen(false) // close the dialog
+    } catch (error) {
+      handleErrorApi({ error, setError: form.setError })
+    }
+  }
 
   return (
     <Dialog onOpenChange={setOpen} open={open}>
@@ -67,6 +109,8 @@ export default function AddEmployee() {
             noValidate
             className="grid auto-rows-max items-start gap-4 md:gap-8"
             id="add-employee-form"
+            onReset={resetForm}
+            onSubmit={form.handleSubmit(onSubmit)}
           >
             <div className="grid gap-4 py-4">
               <FormField
@@ -181,8 +225,14 @@ export default function AddEmployee() {
           </form>
         </Form>
         <DialogFooter>
-          <Button type="submit" form="add-employee-form">
-            Thêm
+          <Button
+            type="submit"
+            form="add-employee-form"
+            disabled={addAccountMutation.isPending}
+          >
+            {addAccountMutation.isPending
+              ? 'Đang tạo tài khoản...'
+              : 'Tạo tài khoản'}
           </Button>
         </DialogFooter>
       </DialogContent>
